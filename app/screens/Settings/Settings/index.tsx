c4af6d7b2a065e03c2c0991f8b63a2ae5e4d86b5
Tejas Gajjar <gajjartejas26@gmail.com>
2024-07-14 19:11:10 +0530

    feat: add translations

--- app/screens/Settings/Settings/index.tsx
+++ app/screens/Settings/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { View } from 'react-native';
 
 //ThirdParty
@@ -31,110 +31,113 @@ const Settings = ({ navigation }: Props) => {
   const selectedLanguageName = useAppLangConfigStore(store => store.selectedLanguageName);
 
   //States
-  const [apps] = useState<ISettingSection[]>([
-    {
-      id: 0,
-      title: t('settings.commonHeader'),
-      items: [
-        {
-          id: 0,
-          iconName: 'language',
-          iconType: 'material',
-          title: t('settings.languageTitle'),
-          description: t('settings.languageSubTitle', {
-            language: selectedLanguageName,
-          })!,
-          route: 'ChangeLanguage',
-        },
-        {
-          id: 1,
-          iconName: 'wb-sunny',
-          iconType: 'material',
-          title: t('settings.appearanceTitle'),
-          description: t('settings.appearanceSubTitle')!,
-          route: 'SelectAppearance',
-        },
-        {
-          id: 2,
-          iconName: 'magnify',
-          iconType: 'material-community',
-          title: t('settings.scanTitle'),
-          description: t('settings.scanSubTitle'),
-          route: 'ScanSetting',
-        },
-        {
-          id: 3,
-          iconName: 'key',
-          iconType: 'material-community',
-          title: t('settings.identitiesTitle'),
-          description: t('settings.identitiesSubTitle'),
-          route: 'Identities',
-        },
-        {
-          id: 4,
-          iconName: 'server-network',
-          iconType: 'material-community',
-          title: t('settings.recentConnectionsTitle'),
-          description: t('settings.recentConnectionsSubTitle'),
-          route: 'Devices',
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: t('settings.infoHeader'),
-      items: [
-        {
-          id: 0,
-          iconName: 'notes',
-          iconType: 'material',
-          title: t('settings.changelogTitle'),
-          description: t('settings.changelogSubTitle')!,
-          route: 'Changelog',
-        },
-        {
-          id: 1,
-          iconName: 'library-shelves',
-          iconType: 'material-community',
-          title: t('settings.librariesTitle'),
-          description: t('settings.librariesSubTitle')!,
-          route: 'License',
-        },
-        {
-          id: 2,
-          iconName: 'frequently-asked-questions',
-          iconType: 'material-community',
-          title: t('settings.faqTitle'),
-          description: t('settings.faqSubTitle')!,
-          route: 'FAQ',
-        },
-        {
-          id: 3,
-          iconName: 'language-outline',
-          iconType: 'ionicon',
-          title: t('settings.translateTitle'),
-          description: t('settings.translateSubTitle')!,
-          route: 'Translate',
-        },
-        {
-          id: 4,
-          iconName: 'people-outline',
-          iconType: 'ionicon',
-          title: t('settings.translatorsTitle'),
-          description: t('settings.translatorsSubTitle')!,
-          route: 'Translators',
-        },
-        {
-          id: 5,
-          iconName: 'privacy-tip',
-          iconType: 'material',
-          title: t('settings.privacyTitle'),
-          description: t('settings.privacySubTitle')!,
-          route: 'PrivacyPolicy',
-        },
-      ],
-    },
-  ]);
+  const apps: ISettingSection[] = useMemo(
+    () => [
+      {
+        id: 0,
+        title: t('settings.commonHeader'),
+        items: [
+          {
+            id: 0,
+            iconName: 'language',
+            iconType: 'material',
+            title: t('settings.languageTitle'),
+            description: t('settings.languageSubTitle', {
+              language: selectedLanguageName,
+            })!,
+            route: 'ChangeLanguage',
+          },
+          {
+            id: 1,
+            iconName: 'wb-sunny',
+            iconType: 'material',
+            title: t('settings.appearanceTitle'),
+            description: t('settings.appearanceSubTitle')!,
+            route: 'SelectAppearance',
+          },
+          {
+            id: 2,
+            iconName: 'magnify',
+            iconType: 'material-community',
+            title: t('settings.scanTitle'),
+            description: t('settings.scanSubTitle'),
+            route: 'ScanSetting',
+          },
+          {
+            id: 3,
+            iconName: 'key',
+            iconType: 'material-community',
+            title: t('settings.identitiesTitle'),
+            description: t('settings.identitiesSubTitle'),
+            route: 'Identities',
+          },
+          {
+            id: 4,
+            iconName: 'server-network',
+            iconType: 'material-community',
+            title: t('settings.recentConnectionsTitle'),
+            description: t('settings.recentConnectionsSubTitle'),
+            route: 'Devices',
+          },
+        ],
+      },
+      {
+        id: 1,
+        title: t('settings.infoHeader'),
+        items: [
+          {
+            id: 0,
+            iconName: 'notes',
+            iconType: 'material',
+            title: t('settings.changelogTitle'),
+            description: t('settings.changelogSubTitle')!,
+            route: 'Changelog',
+          },
+          {
+            id: 1,
+            iconName: 'library-shelves',
+            iconType: 'material-community',
+            title: t('settings.librariesTitle'),
+            description: t('settings.librariesSubTitle')!,
+            route: 'License',
+          },
+          {
+            id: 2,
+            iconName: 'frequently-asked-questions',
+            iconType: 'material-community',
+            title: t('settings.faqTitle'),
+            description: t('settings.faqSubTitle')!,
+            route: 'FAQ',
+          },
+          {
+            id: 3,
+            iconName: 'language-outline',
+            iconType: 'ionicon',
+            title: t('settings.translateTitle'),
+            description: t('settings.translateSubTitle')!,
+            route: 'Translate',
+          },
+          {
+            id: 4,
+            iconName: 'people-outline',
+            iconType: 'ionicon',
+            title: t('settings.translatorsTitle'),
+            description: t('settings.translatorsSubTitle')!,
+            route: 'Translators',
+          },
+          {
+            id: 5,
+            iconName: 'privacy-tip',
+            iconType: 'material',
+            title: t('settings.privacyTitle'),
+            description: t('settings.privacySubTitle')!,
+            route: 'PrivacyPolicy',
+          },
+        ],
+      },
+    ],
+    [selectedLanguageName, t],
+  );
 
   const onGoBack = useCallback(() => {
     navigation.pop();
